9826bf571b2ff770e9fe56cddd07849a4483c3ae
NAV25\uesnav <eliudefrenlopez@gmail.com>
2024-10-28 14:25:37 -0700

    COntador y lista de tareas

---
+++ src/components/Contador.vue
@@ -0,0 +1,23 @@
+<template>
+  <h1>Hola mundo</h1>
+ <p> {{ contador }}</p>
+  <button @click="incremental">incremental</button>
+  <button @click="decrememtal">decrememtal</button>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+const contador = ref(0);
+const incremental = () => {
+  contador.value++;
+}
+const decrememtal = () => {
+  contador.value--;
+}
+</script>
+
+<style scoped>
+p{
+  color: red;
+}
+</style>
